e9489a150c87ab40a833f9d2582ccd59677f6249
hades2007 <87477676+hades2007@users.noreply.github.com>
2021-07-18 13:48:17 +0300

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -91,7 +91,7 @@ client.on("message", msg => {
 });
 /////Reklam////
 client.on('ready' , () => {
-var join = client.channels.get("id voice")/// ئاید ڤۆیس
+var join = client.channels.get("865194823391510528")/// ئاید ڤۆیس
 if (join) join.join()
 })
 
@@ -101,14 +101,14 @@ if(message.channel.type === 'dm') {
  
 if (message.content.startsWith("https://discord.gg/")) {
  
-  message.author.send(`> link server`)/// لینکە سیرڤەرە خۆت
+  message.author.send(`> https://discord.gg/TWjtAMXVRJ`)/// لینکە سیرڤەرە خۆت
  
-  client.channels.get("id text").send(
+  client.channels.get("865194824034680873").send(
 `> Nerdra la layan <@${message.author.id}> 
 ${message.content}`)
 }
     }
         })
 
-client.login("");
+client.login("ODY2MjY1Njc3NjMwNTM3NzI4.YPQCxw.T-FRUtsyLxS2wOIHvwntK7EDIl4");
 
